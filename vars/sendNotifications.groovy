edf674b226eceeabd3d2bbb7da8dab0cde9554a1
Celine Aussourd <celine.aussourd@gmail.com>
2018-06-27 14:42:16 +0100

    add git commits and authors

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -1,6 +1,31 @@
 #!/usr/bin/env groovy
 
 /**
+ * Gather git commits
+ */
+def getChangeString() {
+ MAX_MSG_LEN = 100
+ def changeString = ""
+
+ echo "Gathering SCM changes"
+ def changeLogSets = currentBuild.changeSets
+ for (int i = 0; i < changeLogSets.size(); i++) {
+   def entries = changeLogSets[i].items
+   for (int j = 0; j < entries.length; j++) {
+     def entry = entries[j]
+     truncated_msg = entry.msg.take(MAX_MSG_LEN)
+     changeString += " - ${truncated_msg} [${entry.author}]\n"
+   }
+ }
+
+ if (!changeString) {
+   changeString = " - No new changes"
+ }
+   return changeString
+}
+
+
+/**
  * Send notifications based on build status string
  */
 def call(String buildStatus = 'STARTED') {
@@ -10,7 +35,8 @@ def call(String buildStatus = 'STARTED') {
   // Default values
   def color = 'danger'
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def summary = "${subject} (<${env.BUILD_URL}|Open>)"
+  def changes = getChangeString()
+  def summary = "${subject} (<${env.BUILD_URL}|Open>)\n${changes}"
   def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
 
